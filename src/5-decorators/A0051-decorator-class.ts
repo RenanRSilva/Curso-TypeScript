94bfafc6fd0914025e6ea649f004924b4238bf3d
Renan Silva <renanrambuls@gmail.com>
2021-04-29 13:56:33 -0300

    decorator class

---
+++ src/5-decorators/A0051-decorator-class.ts
@@ -0,0 +1,27 @@
+@decorator //uma função que recebe o target que é a classe que estamos decorando
+export class Animal {
+  constructor(public name: string,  public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T{ //Com o decorator posso intervir no meio da classe
+  return class extends target {
+    cor: string;
+    nome: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('')
+    }
+  };
+}
+
+
+const animal = new Animal('Luiz', 'Roxo');
+console.log(animal);
+
